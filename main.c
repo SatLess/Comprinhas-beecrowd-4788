c49b560e9149747653af18664947b430d94f30a0
SatLess <124409014+SatLess@users.noreply.github.com>
2024-08-13 03:27:41 +0000

    Adiconei retirada de dinheiro da compra produto

--- main.c
+++ main.c
@@ -20,21 +20,30 @@ int checarExistênciaProduto(Prod Carrinho[], char nome[101]){
   return -1;
 }
 
-
-void comprarProduto(Prod Carrinho[], int * idx){
+void comprarProduto(Prod Carrinho[], int *idx, double *budget) {
 
   char nome[101];
   int qtd;
   double preco;
 
-  scanf("%s %d %lf",nome,&qtd,&preco);
-  
-  //Primeiro, checamos se existe um mesmo produto já na lista, caso não, o criamos.
+  scanf("%s %d %lf", nome, &qtd, &preco);
+
+  if (qtd * preco > *budget) {
+    qtd = *budget / preco;
+    printf("%d", qtd);
+  }
+  if (qtd > 0) {
+    *budget -= qtd * preco;
+  }
+
+  // Primeiro, checamos se existe um mesmo produto já na lista, caso não, o
+  // criamos.
   int existe = checarExistênciaProduto(Carrinho, nome);
-    
-  if(existe >= 0){ 
-    Carrinho[existe].qtd+=qtd;
-    printf("%s %d x %lf, idx %d\n",Carrinho[existe].nome,Carrinho[existe].qtd,Carrinho[existe].preco,existe);
+
+  if (existe >= 0) {
+    Carrinho[existe].qtd += qtd;
+    printf("%s %d x %lf, idx %d, dinero: %lf\n", Carrinho[existe].nome,
+           Carrinho[existe].qtd, Carrinho[existe].preco, existe, *budget);
     return;
   }
 
@@ -43,12 +52,11 @@ void comprarProduto(Prod Carrinho[], int * idx){
   Carrinho[*idx].preco = preco;
   Carrinho[*idx].qtd = qtd;
 
-  printf("%s %d x %lf, idx %d\n",Carrinho[*idx].nome,Carrinho[*idx].qtd,Carrinho[*idx].preco,*idx);
-    
-  ++*idx;
-    
-}
+  printf("%s %d x %lf, idx %d, dinero: %lf\n", Carrinho[*idx].nome,
+         Carrinho[*idx].qtd, Carrinho[*idx].preco, *idx, *budget);
 
+  ++*idx;
+}
 
 int main(void) {
 
@@ -61,8 +69,9 @@ int main(void) {
   int idx = 0;
 
   scanf("%d %lf",&numOperações,&budgetInicial);
+  budgetAtual = budgetInicial;
 
-  for(int i = 0; i<numOperações;i++){comprarProduto(carrinho, &idx);}
-  
+  for(int i = 0; i<numOperações;i++){comprarProduto(carrinho, &idx,&budgetAtual);}
+
   return 0;
-}
+  }
